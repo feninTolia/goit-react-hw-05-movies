35ca8fe0867a98c1a7d865d078746de63c9e3bea
feninTolia <fenintolja@gmail.com>
2022-10-18 14:08:27 +0300

    added useFetch

---
+++ src/Service/useFetch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        setData(result);
+      })
+      .catch(err => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  const refetch = () => {
+    setLoading(true);
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        setData(result);
+      })
+      .catch(err => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { data, loading, error, refetch };
+}
+
+export default useFetch;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,20 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import css from './Home.module.css';
-
-const apiTrendingFetch = async () => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/tv/week?api_key=967fca2e12d0ec29fa75f230a5acdce3`
-  );
-  const trendMovies = await response.json();
-  return trendMovies.results;
-};
+import useFetch from '../../Service/useFetch';
 
 const Home = () => {
-  // eslint-disable-next-line
-  const [trendingMovies, setTrendingMovies] = useState([]);
+  const { data, loading, error, refetch } = useFetch(
+    `https://api.themoviedb.org/3/trending/tv/week?api_key=967fca2e12d0ec29fa75f230a5acdce3`
+  );
 
-  useEffect(() => {
-    let ignore = false;
-    setTrendingMovies([]);
-    apiTrendingFetch().then(result => {
-      if (!ignore) {
-        setTrendingMovies(result);
-      }
-    });
-    return () => {
-      ignore = true;
-    };
-  }, []);
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
 
   return (
     <div className={css.trendingMovies}>
-      {trendingMovies.map(el => (
+      {data?.results.map(el => (
         <Link to={`movies/${el.id}`} key={el.id} className={css.moviesList}>
           <img
             src={`https://image.tmdb.org/t/p/w500${el.poster_path}`}
